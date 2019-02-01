e965f48e5cd14e137395329f3bb470dee245645b
Enoka Jaona <Ejaona@gmail.com>
2019-02-01 10:32:34 -0600

    created boiler for reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,17 +2,31 @@
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+// Your initial/default state for this project could *Although does not have to* look a lot like this
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
+
+export const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCHED_DATA:
+      return {};
+    case ADD_SMURF:
+      return {};
+    case DELETE_SMURF:
+      return {};
+    case ERROR:
+      return {};
+    default:
+      return state;
+  }
+};
 
 /*
   You'll only need one smurf reducer for this project.
